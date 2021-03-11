a49bae98529f3e98da852f371ada65df9c275bd9
Matteo Fusillo <matteo@daysix.co>
2021-03-11 16:09:10 +0000

    grab locality from api and use for get doctorslocations

--- src/MapDisplay.js
+++ src/MapDisplay.js
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 import LocationMarker from "./LocationMarker";
-// import LocationMarker from "./LocationMarker";
+import axios from "axios";
 
 export const MapDisplay = () => {
   const [position, setPosition] = useState(null);
+  const [userLocality, setUserLocality] = useState(null);
+  const [doctorsLocations, setDoctorsLocations] = useState([]);
 
-  console.log(position);
+  console.log(userLocality);
+
+  useEffect(() => {
+    if (position) {
+      axios
+        .get(
+          `http://api.positionstack.com/v1/reverse?access_key=ec9f89e4efe3473d6c48f999555798c9&query=${position.lat},${position.lng}`
+        )
+        .then((res) => setUserLocality(res.data.data[0].locality));
+    }
+  }, [position]);
+
+  useEffect(() => {
+    if (userLocality) {
+      axios
+        .get(
+          `http://api.positionstack.com/v1/forward?access_key=ec9f89e4efe3473d6c48f999555798c9&query=medical&region=${userLocality}`
+        )
+        .then((res) => setDoctorsLocations(res.data.data));
+    }
+  }, [userLocality]);
 
   return (
     <MapContainer
@@ -20,6 +42,22 @@ export const MapDisplay = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <LocationMarker position={position} setPosition={setPosition} />
+      {doctorsLocations.length > 0 &&
+        position &&
+        doctorsLocations.map((doctorsLocation, index) => (
+          // <h1 key={index}>{doctorsLocation.longitude}</h1>
+          <Marker
+            key={index}
+            position={[doctorsLocation.latitude, doctorsLocation.longitude]}
+          >
+            <Popup>
+              <div>
+                <h1>{doctorsLocation.label}</h1>
+                <p>{doctorsLocation.label}</p>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
     </MapContainer>
   );
 };
