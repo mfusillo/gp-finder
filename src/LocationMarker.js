6e42ad8861f1b80905b603b67bafa3a74d0153ff
Matteo Fusillo <matteo@daysix.co>
2021-03-11 13:16:25 +0000

    getting position on load with useffect

--- src/LocationMarker.js
+++ src/LocationMarker.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Marker, Popup, useMapEvents } from "react-leaflet";
 
 export const LocationMarker = (props) => {
   const { position, setPosition } = props;
   const map = useMapEvents({
-    click() {
-      map.locate();
-    },
     locationfound(e) {
       setPosition(e.latlng);
       map.flyTo(e.latlng, map.getZoom());
     },
   });
 
+  useEffect(() => {
+    if (!position) {
+      map.locate();
+    }
+  }, []);
   return position === null ? null : (
     <Marker position={position}>
       <Popup>You are here</Popup>
