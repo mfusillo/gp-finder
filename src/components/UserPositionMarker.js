3380f56e0047952168bfc61a303d8c310543dbae
Matteo Fusillo <matteo@daysix.co>
2021-03-13 11:52:30 +0000

    change user icon

--- src/components/UserPositionMarker.js
+++ src/components/UserPositionMarker.js
@@ -10,6 +10,17 @@ export const UserPositionMarker = (props) => {
     },
   });
 
+  const icon = new L.Icon({
+    iconUrl:
+      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
   useEffect(() => {
     if (!position) {
       map.locate();
@@ -17,7 +28,7 @@ export const UserPositionMarker = (props) => {
   }, []);
 
   return position === null ? null : (
-    <Marker position={position}>
+    <Marker icon={icon} position={position}>
       <Popup>You are here</Popup>
     </Marker>
   );
